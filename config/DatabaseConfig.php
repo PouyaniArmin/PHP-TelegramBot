987eeb14aac5d0de201af86ceaed94c327846c96
Pouyani Armin <pouyaniarmin@gmail.com>
2024-08-27 16:44:24 +0330

    update class

--- config/DatabaseConfig.php
+++ config/DatabaseConfig.php
@@ -1,2 +1,44 @@
-<?php 
+<?php
 
+
+namespace Config;
+
+use Dotenv\Dotenv;
+use Exception;
+use MongoDB\Client;
+
+class DatabaseConfig
+{
+
+    private string $mongo_url;
+    private string $tb_name;
+    protected Client $conn;
+    public function __construct()
+    {
+        $this->initDotEnv();
+        $this->conn = $this->connectToMongo();
+    }
+
+
+
+    private function connectToMongo()
+    {
+        try {
+            $clinet = new Client($this->mongo_url);
+            return $clinet;
+        } catch (\Throwable $th) {
+            echo $th->getMessage();
+        }
+    }
+    private function initDotEnv()
+    {
+        $dotenv = Dotenv::createImmutable(dirname(__DIR__));
+        $envFile = $dotenv->safeLoad();
+        if ($envFile) {
+            $this->mongo_url = $_ENV['MONGO_URL'];
+            $this->tb_name = $_ENV['MONGO_TABLE'];
+        } else {
+            return throw new Exception("Error Processing ENV File", 1);
+        }
+    }
+}
---
+++ src/database/MongoDB.php
@@ -0,0 +1,10 @@
+<?php 
+
+namespace Database;
+
+class MongoDB{
+    public function __construct()
+    {
+        echo "Mongo Class";
+    }
+}
