865654bd57cb51f4a0fe1aaf89929cd1880818f6
Pouyani Armin <pouyaniarmin@gmail.com>
2024-08-31 08:59:13 +0330

    Implement MongoDB class to simplify database connections and CRUD operations

--- src/database/MongoDB.php
+++ src/database/MongoDB.php
@@ -2,9 +2,19 @@
 
 namespace Database;
 
-class MongoDB{
-    public function __construct()
+use Config\DatabaseConfig;
+use MongoDB\Client;
+use MongoDB\Collection;
+use Utilities\ErrorHandler;
+
+class MongoDB extends DatabaseConfig{
+    protected Client $clinet;
+    protected Collection $collection;
+    public function __construct(string $db_name,string $collection)
     {
-        echo "Mongo Class";
+        parent::__construct();
+        $this->clinet=$this->getConnection();
+        $this->collection=$this->clinet->selectCollection($db_name,$collection);
     }
+
 }
