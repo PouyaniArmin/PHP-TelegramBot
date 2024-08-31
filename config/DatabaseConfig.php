122301d97cce28811d94080938c14f31ea95ee2d
Pouyani Armin <pouyaniarmin@gmail.com>
2024-08-31 08:47:56 +0330

    Create DatabaseConfig class to initialize environment variables and establish MongoDB connections

--- config/DatabaseConfig.php
+++ config/DatabaseConfig.php
@@ -6,39 +6,49 @@ namespace Config;
 use Dotenv\Dotenv;
 use Exception;
 use MongoDB\Client;
+use Utilities\ErrorHandler;
 
 class DatabaseConfig
 {
 
     private string $mongo_url;
-    private string $tb_name;
-    protected Client $conn;
     public function __construct()
     {
         $this->initDotEnv();
-        $this->conn = $this->connectToMongo();
     }
 
 
 
-    private function connectToMongo()
+    private function connectToMongo(): Client
     {
-        try {
-            $clinet = new Client($this->mongo_url);
-            return $clinet;
-        } catch (\Throwable $th) {
-            echo $th->getMessage();
-        }
+        $client = new Client($this->mongo_url);
+        $client->listDatabases();
+        return $client;
     }
+    /**
+     * load enviroment variable from .env file.
+     * This method uses the Dotenv package to load environment variables from the .env file.
+     * If there is an error loading the .env file or if the required environment variable is missing,
+     * an exception is thrown.
+     * 
+     * @throws Exception if the .env file cannot be loaded or if the MONGO_URL environment variable is not set.
+     */
     private function initDotEnv()
     {
         $dotenv = Dotenv::createImmutable(dirname(__DIR__));
         $envFile = $dotenv->safeLoad();
-        if ($envFile) {
-            $this->mongo_url = $_ENV['MONGO_URL'];
-            $this->tb_name = $_ENV['MONGO_TABLE'];
-        } else {
-            return throw new Exception("Error Processing ENV File", 1);
+        if ($envFile === false) {
+            ErrorHandler::throwException('Error loading the .env file', 1);
         }
+        if (isset($envFile)) {
+            $this->mongo_url = $_ENV['MONGO_URL'];
+        } else {
+            ErrorHandler::throwException('MONGO_URL is not set in the enviroment file');
+        }
+    }
+
+
+    public function getConnection():Client{
+        return $this->connectToMongo();
     }
 }
