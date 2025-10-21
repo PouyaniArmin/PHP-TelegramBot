8d5c187a231ca0de18e2c715e3ac383ccbfa69e4
PouyaniArmin <pouyaniarmin@gmail.com>
2025-10-21 15:23:42 +0330

    Fix bot.php: block GET requests to prevent JSON decode errors

--- public/bot.php
+++ public/bot.php
@@ -4,7 +4,12 @@ use Bot\Api;
 use Bot\TelegramBot;
 use Models\Channels;
 use Models\User;
+
 require_once __DIR__ . "/../vendor/autoload.php";
 
 $telBot = new TelegramBot(new Api, new User, new Channels);
-$telBot->startBot();
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $telBot->startBot();
+} elseif ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    echo "bot is runnig";
+}
